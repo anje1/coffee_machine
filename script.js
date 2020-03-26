c9f0a014813065b3c2dd3ff807160a309b7afaa3
anje1 <anje_qq@mail.ru>
2020-03-26 13:27:09 +0300

    Научили машину готовить кофе

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
 "use strict"
 let balance = document.querySelector(".balance");
+let displayText = document.querySelector(".display-text");
+let progressBar = document.querySelector(".progress-bar");
+let coffeeCup = document.querySelector(".coffee-cup img");
+let coffeeStatus = "waiting"; //"cooking" "ready"
 
-function buyCoffee(name, cost) {
+function buyCoffee(name, cost, elem) {
+    if (coffeeStatus != "waiting") {
+    return;
+    }
   let afterBuyValue = +balance.value - cost;
 //  alert(`Вы заказали ${name}. Цена: ${cost}`);
 //alert(balance.value);
   if ( (balance.value - cost) < 0 || Number.isNaN(afterBuyValue)) {
-    alert("Недостаточно средств!");
+    balance.style.border = "2px solid red";
+    balance.style.backgroundColor = "pink";
+    changeDisplayText("Недостаточно средств!");
     return;
   }
+  balance.style.border = "none";
+  balance.style.backgroundColor = "white";
   balance.value = (+balance.value - cost).toFixed(2);
-  alert("Ваш " + name + " готовится.");
+  cookCoffee(name, elem);
+}
+
+function cookCoffee(name, elem) {
+let cupImg = elem.querySelector("img");
+let cupSrc = cupImg.getAttribute("src");
+coffeeCup.setAttribute("src", cupSrc);
+coffeeCup.style.opacity = "0%";
+coffeeCup.classList.remove("d-none");//убрать класс
+  coffeeStatus = "cooking";
+  changeDisplayText("Ваш " + name + " готовится");
+  //coffeeCup.classList.add(""); //Добавить класс
+ // coffeeCup.classList.toggle(""); // вкл выкл класс
+  //coffeeCup.classList.contains("d-none"); //содержит ли класс
+  
+  let readyPercent = 0;
+  let cookingInterval = setInterval(() => {
+    readyPercent++
+    progressBar.style.width = readyPercent + "%";
+    coffeeCup.style.opacity = readyPercent + "%";
+    if (readyPercent == 100) {
+      coffeeStatus = "ready";
+      changeDisplayText("Ваш " + name + " готов!");
+      coffeeCup.style.cursor = "pointer";
+      clearInterval(cookingInterval);
+    }
+  }, 75);
+}
+
+function changeDisplayText(text) {
+  //displayText.innerText = "<span>"+text+"</span>";
+  displayText.innerHTML = "<span>"+text+"</span>";
 }
 /*способы поиска элементов
 1. в html коде
